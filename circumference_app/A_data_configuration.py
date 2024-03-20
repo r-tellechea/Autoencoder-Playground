841392149bdaec539483f2f78fd089314edbf905
r-tellechea <r.tellechea.job@gmail.com>
2024-03-20 16:48:53 +0100

    Module: Data configuration.

---
+++ circumference_app/A_data_configuration.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+import streamlit as st
+
+from data import generate_data
+from plotly_figures import get_fig_data
+
+def data_configuration(seed: int) -> torch.Tensor:
+
+	column_n_points, column_mean, column_std = st.columns(3)
+
+	with column_n_points:
+		n_points = st.number_input(
+			label='Number of points', 
+			key='n_points',
+			min_value=100, 
+			max_value=10_000, 
+			value=1000,
+		)
+
+	with column_mean:
+		mean = st.slider(
+			label='Radious',
+			key='mean',
+			min_value=1.,
+			max_value=20.,
+			value=10.,
+			step=0.1,
+		)
+
+	with column_std:
+		std = st.slider(
+			label='Deviation',
+			key='std',
+			min_value=0.,
+			max_value=1.,
+			value=0.5,
+			step=0.01,
+		)
+
+	data = generate_data(n_points, mean, std, seed)
+
+	fig_data = get_fig_data(data)
+	st.plotly_chart(fig_data, use_container_width=True)
+
+	return data
--- streamlit_app.py
+++ streamlit_app.py
@@ -21,50 +21,14 @@ seed = st.number_input(
 	max_value=1_000_000
 )
 
-st.subheader('Create the data')
-
 ########################################
 # Data configuration
 ########################################
 
-column_n_points, column_mean, column_std = st.columns(3)
+st.subheader('Create the data')
 
-with column_n_points:
-	n_points = st.number_input(
-		label='Number of points', 
-		key='n_points',
-		min_value=100, 
-		max_value=10_000, 
-		value=1000,
-	)
-
-with column_mean:
-	mean = st.slider(
-		label='Radious',
-		key='mean',
-		min_value=1.,
-		max_value=20.,
-		value=10.,
-		step=0.1,
-	)
-
-with column_std:
-	std = st.slider(
-		label='Deviation',
-		key='std',
-		min_value=0.,
-		max_value=1.,
-		value=0.5,
-		step=0.01,
-	)
-
-# Generate the data
-from data import generate_data
-data = generate_data(n_points, mean, std, seed)
-
-from plotly_figures import get_fig_data
-fig_data = get_fig_data(data)
-st.plotly_chart(fig_data, use_container_width=True)
+from circumference_app.A_data_configuration import data_configuration
+data = data_configuration(seed)
 
 ########################################
 # Neural Networks Configuration
