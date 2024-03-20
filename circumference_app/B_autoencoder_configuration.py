06b7d23e513e76bfdd65df01a36075e30b1acc38
r-tellechea <r.tellechea.job@gmail.com>
2024-03-20 16:53:38 +0100

    Module: Autoencoder configuration.

---
+++ circumference_app/B_autoencoder_configuration.py
@@ -0,0 +1,62 @@
+import torch
+from torch import nn
+import streamlit as st
+
+def autoencoder_configuration(seed: int, data: torch.Tensor) -> tuple[tuple[float]]:
+
+	col_encoder, col_decoder, col_bottleneck = st.columns((2,2,1))
+
+	with col_encoder:
+		n_encoder_hidden_layers = st.slider(
+				label="Number of encoder's hidden layers",
+				key='n_encoder_hidden_layers',
+				min_value=0,
+				max_value=4,
+				value=3,
+				step=1,
+			)
+
+	with col_decoder:	
+		n_decoder_hidden_layers = st.slider(
+				label="Number of decoder's hidden layers",
+				key='n_decoder_hidden_layers',
+				min_value=0,
+				max_value=4,
+				value=3,
+				step=1,
+			)
+
+	with col_bottleneck:
+		n_bottleneck_neurons = st.radio(
+			label='Bottleneck neurons',
+			key='bottleneck_neurons',
+			options=[1,2],
+		)
+
+	list_encoder_layer_size = [1] * n_encoder_hidden_layers
+	list_decoder_layer_size = [1] * n_decoder_hidden_layers
+
+	with col_encoder:
+		for layer in range(n_encoder_hidden_layers):
+			list_encoder_layer_size[layer] = st.number_input(
+				label=f"Neurons in encoder's layer {layer+1}",
+				key=f'encoder_{layer}_size',
+				min_value=1,
+				max_value=8,
+				value=8,
+			)
+
+	with col_decoder:
+		for layer in range(n_decoder_hidden_layers):
+			list_decoder_layer_size[layer] = st.number_input(
+				label=f"Neurons in decoder's layer {layer+1}",
+				key=f'decoder_{layer}_size',
+				min_value=1,
+				max_value=8,
+				value=8,
+			)
+
+	encoder_arch = tuple(list_encoder_layer_size)
+	decoder_arch = tuple(list_decoder_layer_size)
+
+	return encoder_arch, decoder_arch, n_bottleneck_neurons
--- streamlit_app.py
+++ streamlit_app.py
@@ -35,60 +35,9 @@ data = data_configuration(seed)
 ########################################
 
 st.subheader('Autoencoder architecture')
-col_encoder, col_decoder, col_bottleneck = st.columns((2,2,1))
 
-with col_encoder:
-	n_encoder_hidden_layers = st.slider(
-			label="Number of encoder's hidden layers",
-			key='n_encoder_hidden_layers',
-			min_value=0,
-			max_value=4,
-			value=3,
-			step=1,
-		)
-
-with col_decoder:	
-	n_decoder_hidden_layers = st.slider(
-			label="Number of decoder's hidden layers",
-			key='n_decoder_hidden_layers',
-			min_value=0,
-			max_value=4,
-			value=3,
-			step=1,
-		)
-
-with col_bottleneck:
-	n_bottleneck_neurons = st.radio(
-		label='Bottleneck neurons',
-		key='bottleneck_neurons',
-		options=[1,2],
-	)
-
-list_encoder_layer_size = [1] * n_encoder_hidden_layers
-list_decoder_layer_size = [1] * n_decoder_hidden_layers
-
-with col_encoder:
-	for layer in range(n_encoder_hidden_layers):
-		list_encoder_layer_size[layer] = st.number_input(
-			label=f"Neurons in encoder's layer {layer+1}",
-			key=f'encoder_{layer}_size',
-			min_value=1,
-			max_value=8,
-			value=8,
-		)
-
-with col_decoder:
-	for layer in range(n_decoder_hidden_layers):
-		list_decoder_layer_size[layer] = st.number_input(
-			label=f"Neurons in decoder's layer {layer+1}",
-			key=f'decoder_{layer}_size',
-			min_value=1,
-			max_value=8,
-			value=8,
-		)
-
-encoder_arch = tuple(list_encoder_layer_size)
-decoder_arch = tuple(list_decoder_layer_size)
+from circumference_app.B_autoencoder_configuration import autoencoder_configuration
+encoder_arch, decoder_arch, n_bottleneck_neurons = autoencoder_configuration(seed, data)
 
 # TODO: Temporal.
 st.text(f'Autoencoder architecture: {(2,) + encoder_arch + (n_bottleneck_neurons,) + decoder_arch + (2,)}')
